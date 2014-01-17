a38cbfa7a42df6ac1db162b3638fdd8b32885032
xhan <kfhanjie@163.com>
2014-01-17 14:01:45 +0800

    commit again

--- core/src/main/java/com/github/jremoting/core/App.java
+++ core/src/main/java/com/github/jremoting/core/App.java
@@ -2,6 +2,6 @@ package com.github.jremoting.core;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("hello github!");
+		System.out.println("hello github! good!");
 	}
 }
